7b4033b44676272092365cd924fdf4ebfd859aac
JacobFYeomans <90638405+JacobFYeomans@users.noreply.github.com>
2021-11-15 12:16:37 -0400

    Intoduced player string input(player choice)

--- Program.cs
+++ Program.cs
@@ -8,9 +8,24 @@ namespace Interactive
 {
     class Program
     {
+        static int pageNumber = 0;
+        static string input;
+        static bool acceptingInput = false;
+        static void PlayerInput()
+        {
+            acceptingInput = true;
+            while (acceptingInput == true)
+            {
+                input = Console.ReadLine();
+                Console.WriteLine(input);
+                break;
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Interactive Story");
+
+            PlayerInput();
 
             Console.ReadKey(true);
         }
