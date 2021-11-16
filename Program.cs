5516f0b5bf6ee7ebc14a89b5711162934f3f99fc
JacobFYeomans <90638405+JacobFYeomans@users.noreply.github.com>
2021-11-16 09:39:00 -0400

    commented out input while loop in favour of using switches

--- Program.cs
+++ Program.cs
@@ -12,54 +12,54 @@ namespace Interactive
         static string input;
         static bool acceptingInput = false;
         static string[] story = new string[10];
-        static void PlayerInput()
-        {
-            acceptingInput = true;
-            input = Console.ReadLine();
-            while (acceptingInput == true)
-            {
-                if (input == "=") // pause/menu button is >>> = <<<
-                {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("        Pause Menu");
-                    Console.WriteLine("To save game, enter 'S' key");
-                    Console.WriteLine("To load game, enter 'L' key");
-                    Console.WriteLine("To exit game, enter 'X' key");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    input = Console.ReadLine();
-                    if (input == "S")
-                    {
-                        //lmao this is going to be annoying
-                        acceptingInput = false;
-                        break;
-                    }
-                    if (input == "L")
-                    {
-                        // ^^^^^^^^
-                        acceptingInput = false;
-                        break;
-                    }
-                    if (input == "X")
-                    {
-                        Environment.Exit(0);
-                    }
-                }
-                if (input == "Q")
-                {
-                    Page(pageNumber);
-                    acceptingInput = false;
-                    break;
-                }
-                acceptingInput = false;
-                break;
-            }
-        }
+        //static void PlayerInput()
+        //{
+           // acceptingInput = true;
+            //input = Console.ReadLine();
+            //while (acceptingInput == true)
+            //{
+                //if (input == "=") // pause/menu button is >>> = <<<
+                //{
+                    //Console.ForegroundColor = ConsoleColor.Blue;
+                    //Console.WriteLine("        Pause Menu");
+                    //Console.WriteLine("To save game, enter 'S' key");
+                    //Console.WriteLine("To load game, enter 'L' key");
+                    //Console.WriteLine("To exit game, enter 'X' key");
+                    //Console.ForegroundColor = ConsoleColor.White;
+                    //input = Console.ReadLine();
+                    //if (input == "S")
+                    //{
+                        ////lmao this is going to be annoying
+                        //acceptingInput = false;
+                        //break;
+                    //}
+                    //if (input == "L")
+                    //{
+                        //// ^^^^^^^^
+                        //acceptingInput = false;
+                        //break;
+                    //}
+                    //if (input == "X")
+                    //{
+                        //Environment.Exit(0);
+                    //}
+                //}
+                //if (input == "Q")
+                //{
+                    //Page(pageNumber);
+                    //acceptingInput = false;
+                    //break;
+                //}
+                //acceptingInput = false;
+                //break;
+            //}
+        //}
         static void Introduction()
         {
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Welcome to a variable tale. In the course of your journey, you will make many choices that will define your story.");
             Console.WriteLine("                    Now, make your first choice... will you start your adventure?");
-            Console.WriteLine("                                      Press 'Q' to Start");
+            Console.WriteLine("                                      Press '1' to Start");
             Console.ForegroundColor = ConsoleColor.White;
         }
         static void Page(int page)
@@ -67,11 +67,40 @@ namespace Interactive
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("PAGE: " + page);
             Console.WriteLine();
-            Console.WriteLine(story[0]);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(story[page - 1]);
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
+        }
+        static void Choice()
+        {
+            Console.WriteLine("Make a choice 1, 2, 3, or 4");
+            input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+
+                    Page(pageNumber);
+                    break;
+
+                case "2":
+
+                    break;
+
+                case "3":
+
+                    break;
+
+                case "4":
+
+                    break;
+
+                default:
+
+                    Console.WriteLine("Input not recognized, please chose option 1-4");
+                    Choice();
+                    break;
+            }
         }
         static void Main(string[] args)
         {
@@ -95,7 +124,7 @@ namespace Interactive
             Console.WriteLine();
 
             Introduction();
-            PlayerInput();
+            Choice();
 
             Console.ReadKey(true);
         }
