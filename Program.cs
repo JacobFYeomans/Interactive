f57b3c6f4a7e46b44b4e2d78879e22652d59be3d
JacobFYeomans <90638405+JacobFYeomans@users.noreply.github.com>
2021-11-23 13:22:15 -0400

    All routes lead to end page

--- Program.cs
+++ Program.cs
@@ -108,12 +108,12 @@ namespace Interactive
             story[0] = "You come accross a split path.;Go left.;Go right.;2;3"; //page 1
             story[1] = "After walking some time to the left, you come accross another split path.;Continue left.;Go right.;4;5"; //page 2
             story[2] = "After walking some time to the right, you come accross a large tree.;Continue past the tree.;Inspect the tree.;6;7"; //page 3
-            story[3] = "You go left, the path is a dead end. Game over."; //page 4
-            story[4] = "You go right, and find a pot of gold. You Win"; //page 5
-            story[5] = "You continue past the tree are are attacked by a massive spider. Game Over."; //page 6
+            story[3] = "You go left, the path is a dead end. Game over.;delete;this;10;10"; //page 4
+            story[4] = "You go right, and find a pot of gold. You Win.;delete;this;10;10"; //page 5
+            story[5] = "You continue past the tree are are attacked by a massive spider. Game Over.;delete;this;10;10"; //page 6
             story[6] = "You inspect the Tree, you see ancient text engraved on it.;Decypher the text.;Ignore it.;8;9"; //page 7
-            story[7] = "The text tells you there's a large pot of gold on the other path, and that you wasted your time."; //page 8
-            story[8] = "You ignore the text, and the tree comes to life and eats you. Game Over."; //page 9
+            story[7] = "The text tells you there's a large pot of gold on the other path, and that you wasted your time.;delete;this;10;10"; //page 8
+            story[8] = "You ignore the text, and the tree comes to life and eats you. Game Over.;delete;this;10;10"; //page 9
             story[9] = "J: End Page"; //page 10
             Console.WriteLine("");
             Console.WriteLine("");
