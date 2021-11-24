6eaf22ade87c44560a053c88163761306e5a47cd
JacobFYeomans <90638405+JacobFYeomans@users.noreply.github.com>
2021-11-24 15:36:28 -0400

    Added 2 new method to make game loop cleaner

--- Program.cs
+++ Program.cs
@@ -27,6 +27,27 @@ namespace Interactive
             Console.WriteLine("");
             Console.ForegroundColor = ConsoleColor.White;
         } //Code above ^^^^^^ is probably useless code, not deleting because it might be useful, but it is unlikely.
+        static void PrintIntroInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("A Short Tale of Variable Adventures");
+            Console.WriteLine("By: Jacob. F. Yeomans");
+            //Console.WriteLine("To access menu, enter '='");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void StoryInitializtion()
+        {
+            story [0] = "You come accross a split path.;Go left.;Go right.;2;3"; //page 1
+            story [1] = "After walking some time to the left, you come accross another split path.;Continue left.;Go right.;4;5"; //page 2
+            story [2] = "After walking some time to the right, you come accross a large tree.;Continue past the tree.;Inspect the tree.;6;7"; //page 3
+            story [3] = "You go left, the path is a dead end. Game over.;delete;this;10;10"; //page 4
+            story [4] = "You go right, and find a pot of gold. You Win.;delete;this;10;10"; //page 5
+            story [5] = "You continue past the tree are are attacked by a massive spider. Game Over.;delete;this;10;10"; //page 6
+            story [6] = "You inspect the Tree, you see ancient text engraved on it.;Decypher the text.;Ignore it.;8;9"; //page 7
+            story [7] = "The text tells you there's a large pot of gold on the other path, and that you wasted your time.;delete;this;10;10"; //page 8
+            story [8] = "You ignore the text, and the tree comes to life and eats you. Game Over.;delete;this;10;10"; //page 9
+            story [9] = "J: End Page"; //page 10
+        }
         static void PrintPage(int page) //calls the actual story
         {
             // Console.WriteLine(input); this calls on the input in StoryChoice(); and thus can be used to determine if choice 1 or 2 was chosen.
@@ -104,21 +125,8 @@ namespace Interactive
         } //to chose start/quit on game load, will eventually include load from save.
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("A Short Tale of Variable Adventures");
-            Console.WriteLine("By: Jacob. F. Yeomans");
-            //Console.WriteLine("To access menu, enter '='");
-            Console.ForegroundColor = ConsoleColor.White;
-            story[0] = "You come accross a split path.;Go left.;Go right.;2;3"; //page 1
-            story[1] = "After walking some time to the left, you come accross another split path.;Continue left.;Go right.;4;5"; //page 2
-            story[2] = "After walking some time to the right, you come accross a large tree.;Continue past the tree.;Inspect the tree.;6;7"; //page 3
-            story[3] = "You go left, the path is a dead end. Game over.;delete;this;10;10"; //page 4
-            story[4] = "You go right, and find a pot of gold. You Win.;delete;this;10;10"; //page 5
-            story[5] = "You continue past the tree are are attacked by a massive spider. Game Over.;delete;this;10;10"; //page 6
-            story[6] = "You inspect the Tree, you see ancient text engraved on it.;Decypher the text.;Ignore it.;8;9"; //page 7
-            story[7] = "The text tells you there's a large pot of gold on the other path, and that you wasted your time.;delete;this;10;10"; //page 8
-            story[8] = "You ignore the text, and the tree comes to life and eats you. Game Over.;delete;this;10;10"; //page 9
-            story[9] = "J: End Page"; //page 10
+            PrintIntroInfo();
+            StoryInitializtion();
             Console.WriteLine("");
             Console.WriteLine("");
             Console.WriteLine("");
